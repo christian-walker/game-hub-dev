8d1600ab42ebd372ee21080833361a774780e211
Christian Walker <create@christianwalker.dev>
2025-06-12 12:00:25 -0500

    Downgraded chakra version and built responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Show } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react"
 
 function App() {
 
   return (
    <>
-   <Box p="4" color="white" bg="blue.500">
-      Hello World
-   </Box>
-   <Button>Click Me</Button>
-
+    <Grid templateAreas={
+      {
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }
+    }>
+      <GridItem area="nav" bg="coral">Nav</GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">Aside</GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">Main</GridItem>
+    </Grid>
    </>
   )
 }
