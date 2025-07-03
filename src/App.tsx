639b76d1bf877c6326d576897aa745eff99eef5a
Christian Walker <create@christianwalker.dev>
2025-07-03 12:58:37 -0500

    Show Genre Skeleton while fetching the genres

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,16 @@ function App() {
       {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
-      }
-    }>
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '200px 1fr'
+      }}>
       <GridItem area="nav" p={4}>
           <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" p={5}>
           <GenreList></GenreList>
         </GridItem>
       </Show>
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,19 +1,36 @@
-import { Heading, VStack } from '@chakra-ui/react';
+import { Heading, HStack, VStack, Text, Image, Skeleton, SkeletonText } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
-import GenreCard from './GenreCard';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
-    const {data} = useGenres();
+    const {data, loading, error} = useGenres();
+    const skeletonCount = [1,2,3,4,5,6,7,8,9, 10]; // Number of skeletons to show when loading
+
+    if(error) return null
+    
   return (
-    <VStack justifyContent="start" alignItems="start" padding={5}>
-        
+    <>
         <Heading fontSize="2xl" marginBottom={2}>Genres</Heading>
-        {
-            data.map((genre) => (
-                <GenreCard key={genre.id} genre={genre} />
-            ))
+        <VStack justifyContent="start" alignItems="start">
+        {loading && 
+            
+              skeletonCount.map((skeleton) => (
+                <HStack key={skeleton}>
+                  <Skeleton height="32px" width="32px" borderRadius={8} marginRight={2}/>
+                  <SkeletonText />
+                </HStack>
+              ) )
         }
-    </VStack>
+                        {
+                data.map((genre) => (
+                  <HStack key={genre.id} >
+                  <Image src={getCroppedImageUrl(genre.image_background)} boxSize="32px" borderRadius={8} objectFit="cover" marginRight={2} />
+                  <Text>{genre.name}</Text>
+              </HStack>
+                ))
+            }
+        </VStack>
+    </>
   )
 }
 
